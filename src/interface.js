c75b15f4b6c3591745ea911cac471cf7e6dccac0
Daniel Ridgway <ridginald@gmail.com>
2018-06-15 13:24:46 +0100

    added APIs

--- src/interface.js
+++ src/interface.js
@@ -1,37 +1,52 @@
 $(document).ready(function() {
-    var thermostat = new Thermostat();
-    updateTemperture();
+  var thermostat = new Thermostat();
+  updateTemperture();
 
-    $('#temp-up').click(function() {
-        thermostat.up();
-        updateTemperture();
-    });
+  $('#current-city').change(function(){
+      var city = $('#current-city').val();
+      var url = 'http://api.openweathermap.org/data/2.5/weather?q='
+      var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed'
+      var units = '&units=metric'
 
-    $('#temp-down').click(function() {
-        thermostat.down();
-        updateTemperture();
-    });
+      $.get(url + city + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+      });
+  });
 
-    $('#temp-reset').click(function() {
-        thermostat.resetTemperature();
-        updateTemperture();
-    });
+  $('#temp-up').click(function() {
+      thermostat.up();
+      updateTemperture();
+  })
 
-    $('#psm-on').click(function() {
-        thermostat.switchPowerSavingModeOn();
-        $('#power-saving-status').text('on');
-        updateTemperture();
-    });
+  $('#temp-down').click(function() {
+      thermostat.down();
+      updateTemperture();
+  })
 
-    $('#psm-off').click(function() {
-        thermostat.switchPowerSavingModeOff();
-        $('#power-saving-status').text('off')
-        updateTemperture();
-    });
+  $('#temp-reset').click(function() {
+      thermostat.resetTemperature();
+      updateTemperture();
+  })
 
-    function updateTemperture() {
-        $('#temperature').text(thermostat.temperature);
-        $('#temperature').attr('class', thermostat.energyUsage());
-    };
+  $('#psm-on').click(function() {
+      thermostat.switchPowerSavingModeOn();
+      $('#power-saving-status').text('on');
+      updateTemperture();
+  })
+
+  $('#psm-off').click(function() {
+      thermostat.switchPowerSavingModeOff();
+      $('#power-saving-status').text('off')
+      updateTemperture();
+  })
+
+  function updateTemperture() {
+      $('#temperature').text(thermostat.temperature);
+      $('#temperature').attr('class', thermostat.energyUsage());
+  }
+
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      $('#current-temperature').text(data.main.temp);
+  })
 
 });
